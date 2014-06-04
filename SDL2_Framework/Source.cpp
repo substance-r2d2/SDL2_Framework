97f92245275c343bdf096eef2a30bf4db6d52114
substance-r2d2 <ananddhavle@gmail.com>
2014-06-04 16:20:17 +0530

    Displayed a window

---
+++ SDL2_Framework/Source.cpp
@@ -0,0 +1,33 @@
+#include <SDL.h>
+#include <iostream>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		g_pWindow = SDL_CreateWindow("ver1.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	}
+
+	if (g_pWindow != 0)
+	{
+		g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+	}
+	else
+		return 1;
+
+	SDL_SetRenderDrawColor(g_pRenderer, 255, 255, 255, 255);
+
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_RenderPresent(g_pRenderer);
+
+	SDL_Delay(5000);
+
+	SDL_Quit();
+
+	return 0;
+}
+
